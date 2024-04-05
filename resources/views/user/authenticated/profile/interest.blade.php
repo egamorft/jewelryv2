807b8ba7e31a2f7b11e546b00b546e38cef4e478
Egamorft <huytung.htbn@gmail.com>
2024-04-05 09:29:15 +0700

    product interest & membership benefit

---
+++ resources/views/user/authenticated/profile/interest.blade.php
@@ -0,0 +1,173 @@
+@extends('user.authenticated.profile.layouts.profile')
+
+@section('my-shop-content')
+    <div id="drmvsn-basic-container">
+        <div class="title-area">
+            <span class="mobile-only go-back"></span>
+            <h2>PRODUCT OF INTEREST</h2>
+        </div>
+
+        <div class="xans-element- xans-myshop xans-myshop-wishlist xans-record-">
+            <div class="sub-title pb-10 font-bold flex align-center justify-between">
+                <div class="title">List of products of interest</div>
+            </div>
+            <div class=" ec-base-product">
+                <ul class="xans-element- xans-myshop xans-myshop-wishlistitem wish-list mt-20 flex flex-wrap gap-10">
+                    <li class="prd-card wish-item soldout-displaynone-when-has-stock mb-30 xans-record- on-extra-sale">
+                        <div class="thumbnail">
+                            <a href="#"><img
+                                    src="//www.dona-d.com/web/product/medium/202402/70ca85464490514cdeae31abc278e8c3.jpg"
+                                    alt="" class="loaded"></a>
+                            <div class="wish-del">
+                                <a href="#none" class="btn_wishlist_del" rel="6518||||">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15.167" height="14"
+                                        viewBox="0 0 15.167 14">
+                                        <path
+                                            d="M10.958,18.5l-1.1-.992C5.953,13.922,3.375,11.6,3.375,8.7A4.124,4.124,0,0,1,7.546,4.5a4.486,4.486,0,0,1,3.412,1.6,4.486,4.486,0,0,1,3.412-1.6,4.124,4.124,0,0,1,4.171,4.2c0,2.9-2.578,5.226-6.484,8.812Z"
+                                            transform="translate(-3.375 -4.5)"></path>
+                                    </svg></a>
+                            </div>
+                        </div>
+                        <div class="description mt-20">
+                            <div>
+                                <a href="#">Donna Andy Classic Black Diamond Tennis Bracelet [4mm]</a>
+                            </div>
+
+                            <ul class="spec font-bold mt-5 applied">
+                                <li class="prd-customer-price font-bold">
+                                    <span>
+                                        3,900,000 won<br>
+                                    </span>
+                                </li>
+                            </ul>
+                            <div class="btns mt-20">
+                                <a href="#" class="btn prior-2 is-small ">Put in a shopping cart</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="prd-card wish-item soldout-displaynone-when-has-stock mb-30 xans-record- on-extra-sale">
+                        <div class="thumbnail">
+                            <a href="#"><img
+                                    src="//www.dona-d.com/web/product/medium/202402/70ca85464490514cdeae31abc278e8c3.jpg"
+                                    alt="" class="loaded"></a>
+                            <div class="wish-del">
+                                <a href="#none" class="btn_wishlist_del" rel="6518||||">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15.167" height="14"
+                                        viewBox="0 0 15.167 14">
+                                        <path
+                                            d="M10.958,18.5l-1.1-.992C5.953,13.922,3.375,11.6,3.375,8.7A4.124,4.124,0,0,1,7.546,4.5a4.486,4.486,0,0,1,3.412,1.6,4.486,4.486,0,0,1,3.412-1.6,4.124,4.124,0,0,1,4.171,4.2c0,2.9-2.578,5.226-6.484,8.812Z"
+                                            transform="translate(-3.375 -4.5)"></path>
+                                    </svg></a>
+                            </div>
+                        </div>
+                        <div class="description mt-20">
+                            <div>
+                                <a href="#">Donna Andy Classic Black Diamond Tennis Bracelet [4mm]</a>
+                            </div>
+
+                            <ul class="spec font-bold mt-5 applied">
+                                <li class="prd-customer-price font-bold">
+                                    <span>
+                                        3,900,000 won<br>
+                                    </span>
+                                </li>
+                            </ul>
+                            <div class="btns mt-20">
+                                <a href="#" class="btn prior-2 is-small ">Put in a shopping cart</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="prd-card wish-item soldout-displaynone-when-has-stock mb-30 xans-record- on-extra-sale">
+                        <div class="thumbnail">
+                            <a href="#"><img
+                                    src="//www.dona-d.com/web/product/medium/202402/70ca85464490514cdeae31abc278e8c3.jpg"
+                                    alt="" class="loaded"></a>
+                            <div class="wish-del">
+                                <a href="#none" class="btn_wishlist_del" rel="6518||||">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15.167" height="14"
+                                        viewBox="0 0 15.167 14">
+                                        <path
+                                            d="M10.958,18.5l-1.1-.992C5.953,13.922,3.375,11.6,3.375,8.7A4.124,4.124,0,0,1,7.546,4.5a4.486,4.486,0,0,1,3.412,1.6,4.486,4.486,0,0,1,3.412-1.6,4.124,4.124,0,0,1,4.171,4.2c0,2.9-2.578,5.226-6.484,8.812Z"
+                                            transform="translate(-3.375 -4.5)"></path>
+                                    </svg></a>
+                            </div>
+                        </div>
+                        <div class="description mt-20">
+                            <div>
+                                <a href="#">Donna Andy Classic Black Diamond Tennis Bracelet [4mm]</a>
+                            </div>
+
+                            <ul class="spec font-bold mt-5 applied">
+                                <li class="prd-customer-price font-bold">
+                                    <span>
+                                        3,900,000 won<br>
+                                    </span>
+                                </li>
+                            </ul>
+                            <div class="btns mt-20">
+                                <a href="#" class="btn prior-2 is-small ">Put in a shopping cart</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="prd-card wish-item soldout-displaynone-when-has-stock mb-30 xans-record- on-extra-sale">
+                        <div class="thumbnail">
+                            <a href="#"><img
+                                    src="//www.dona-d.com/web/product/medium/202402/70ca85464490514cdeae31abc278e8c3.jpg"
+                                    alt="" class="loaded"></a>
+                            <div class="wish-del">
+                                <a href="#none" class="btn_wishlist_del" rel="6518||||">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15.167" height="14"
+                                        viewBox="0 0 15.167 14">
+                                        <path
+                                            d="M10.958,18.5l-1.1-.992C5.953,13.922,3.375,11.6,3.375,8.7A4.124,4.124,0,0,1,7.546,4.5a4.486,4.486,0,0,1,3.412,1.6,4.486,4.486,0,0,1,3.412-1.6,4.124,4.124,0,0,1,4.171,4.2c0,2.9-2.578,5.226-6.484,8.812Z"
+                                            transform="translate(-3.375 -4.5)"></path>
+                                    </svg></a>
+                            </div>
+                        </div>
+                        <div class="description mt-20">
+                            <div>
+                                <a href="#">Donna Andy Classic Black Diamond Tennis Bracelet [4mm]</a>
+                            </div>
+
+                            <ul class="spec font-bold mt-5 applied">
+                                <li class="prd-customer-price font-bold">
+                                    <span>
+                                        3,900,000 won<br>
+                                    </span>
+                                </li>
+                            </ul>
+                            <div class="btns mt-20">
+                                <a href="#" class="btn prior-2 is-small ">Put in a shopping cart</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="xans-element- xans-myshop xans-myshop-wishlistpaging ec-base-paginate">
+            <!-- product and basic pagination -->
+            <div class="flex justify-center align-center">
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="38" viewBox="0 0 48 38">
+                        <g transform="translate(-877.5 -600)">
+                            <path d="M-19057.816-16469.5l-5,5,5,5" transform="translate(19961.816 17083)" fill="none"
+                                stroke="#000" stroke-width="1"></path>
+                            <rect width="48" height="38" transform="translate(877.5 600)" fill="none"></rect>
+                        </g>
+                    </svg></a>
+                <ol class="flex justify-center align-center">
+                    <li class="xans-record-"><a href="?page=1" class="this">1</a></li>
+                </ol>
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="38" viewBox="0 0 48 38">
+                        <g transform="translate(-540 -859)">
+                            <path d="M-19062.814-16469.5l5,5-5,5" transform="translate(19624.314 17342)" fill="none"
+                                stroke="#000" stroke-width="1"></path>
+                            <rect width="48" height="38" transform="translate(540 859)" fill="none"></rect>
+                        </g>
+                    </svg></a>
+            </div>
+        </div>
+    </div>
+@endsection
